fddc3600e02c563872817db296e2d0030ef67b99
sgavela <33860117+sgavela@users.noreply.github.com>
2021-04-23 16:48:53 +0200

    commit vectores

--- ast/e/ListIndex.java
+++ ast/e/ListIndex.java
@@ -2,31 +2,32 @@ package ast.e;
 
 import ast.t.Tipo;
 import java.util.ArrayList;
+import java.util.ArrayDeque;
 
 public class ListIndex extends Expresion {
     private Id id;
-    private Expresion idx;
+    private ArrayDeque<Expresion> idx;
     private Tipo tipoLista;
     
-    public ListIndex(Id id, Expresion idx) {
+    public ListIndex(Id id, ArrayDeque<Expresion> idx) {
         this.id = id;
         this.idx = idx;
         this.tipoLista = null;
     }
     
-    public ListIndex(Id id, Expresion idx, Tipo tipoLista) {
+    public ListIndex(Id id, ArrayDeque<Expresion> idx, Tipo tipoLista) {
         this.id = id;
         this.idx = idx;
         this.tipoLista = tipoLista;
     }
     
     public String toString() {
-        String s = id.toString() + '[' + idx.toString() + ']';
+        String s = id.toString() + idx.toString();
         return s;
     }
 
     public String toString(int prof, ArrayList<Boolean> niveles) {
-        String s = id.toString() + '[' + idx.toString() + ']';
+        String s = id.toString() + idx.toString();
         return s;
     }
 
