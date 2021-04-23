7781070f6750d89ae77282b2d3697382dac85ba1
manu_or <manuor@safgd>
2021-04-23 13:18:13 +0200

    Punteros y reserva memoria

--- ast/i/InstruccionReservaMemoria.java
+++ ast/i/InstruccionReservaMemoria.java
@@ -1,15 +1,15 @@
 package ast.i;
 
-import ast.t.Tipo;
+import ast.e.Id;
 import java.util.ArrayList;
 
 public class InstruccionReservaMemoria extends Instruccion {
-    private Tipo tipo;
+    private Id id;
     private int tam;
     
-    public InstruccionReservaMemoria(Tipo tipo, int tam) {
-        this.tipo = tipo;
-        this.tam = tam;
+    public InstruccionReservaMemoria(Id id, String tam) {
+        this.id = id;
+        this.tam = Integer.parseInt(tam);
     }
     
     public TipoInstruccion getTipo() {
@@ -17,7 +17,21 @@ public class InstruccionReservaMemoria extends Instruccion {
     }
     
     public String toString(int prof, ArrayList<Boolean> niveles) {
-        String s = "{{_Reserva_}{" + tipo + ',' + tam + "}}";
+        String s = "";
+        String b_prof = "";
+        if(niveles.size() == prof) niveles.add(true);
+        else niveles.set(prof,true);
+        for(int i=0; i<prof-1; ++i){
+            if(niveles.get(i)) b_prof += '|';
+            b_prof += "   ";
+        }
+        b_prof += '|';
+        if(niveles.size() == prof+1) niveles.add(false);
+        else niveles.set(prof+1,false);
+        s += b_prof + "---{Reserva memoria}\n";
+        s += b_prof + "   " + "|" + "---" + "Identificador: " + id.toString() + '\n';
+        s += b_prof + "   " + "|" + "---" + "Espacio: " + tam + '\n';
+        niveles.set(prof,false);
         return s;
     }
 }
